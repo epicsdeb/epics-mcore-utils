2a4fc00c90bb7755fa8adbd64c326d5a4ac7ef52
Martin Konrad <konrad@frib.msu.edu>
2018-12-28 13:11:13 -0500

    Customize debian directory

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(epics-mcore-utils):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="epics-mcore-utils" command="/usr/bin/epics-mcore-utils"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libepics-mcore-utils 1.2.1 epics-mcore-utils (>> 1.2.1-0), epics-mcore-utils (<< 1.2.1-99)
